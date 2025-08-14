6c3e012aacdcfca70253f3705ce03007ac5c4d5d
Alex <alex.yarotski@gmail.com>
2025-08-14 20:29:44 +0300

    Import assets

--- test/assets/script/game/CarController.ts
+++ test/assets/script/game/CarController.ts
@@ -22,31 +22,36 @@ export default class CarController extends Component {
         this.collider.off('onCollisionExit', this.onCollisionExit, this);
     }
 
-    private onCollisionEnter(e: ICollisionEvent): void {
-        const logComponent = e.otherCollider.node.getComponent(Log);
-        if (logComponent) {
-            // Проверяем наличие бревна в массиве через findIndex
-            const alreadyExists = this.logsInContact.findIndex(log => log === logComponent) !== -1;
-            if (!alreadyExists) {
-                this.logsInContact.push(logComponent);
-                console.log(`Бревно добавлено. Всего бревен: ${this.logsInContact.length}`);
-            }
+    private onCollisionEnter(event: ICollisionEvent): void {
+        const logComponent = event.otherCollider.node.getComponent(Log);
+
+        if (!logComponent) {
+            return;
+        }
+
+        const alreadyExists = this.logsInContact.findIndex(log => log === logComponent) !== -1;
+
+        if (!alreadyExists) {
+            this.logsInContact.push(logComponent);
+            console.log(`Бревно добавлено. Всего бревен: ${this.logsInContact.length}`);
         }
     }
 
-    private onCollisionExit(e: ICollisionEvent): void {
-        const logComponent = e.otherCollider.node.getComponent(Log);
-        if (logComponent) {
-            // Удаляем бревно из массива
-            const index = this.logsInContact.findIndex(log => log === logComponent);
-            if (index !== -1) {
-                this.logsInContact.splice(index, 1);
-                console.log(`Бревно удалено. Осталось бревен: ${this.logsInContact.length}`);
+    private onCollisionExit(event: ICollisionEvent): void {
+        const logComponent = event.otherCollider.node.getComponent(Log);
 
-                // Проверяем, если массив пуст
-                if (this.logsInContact.length === 0) {
-                    console.log("Test");
-                }
+        if (!logComponent) {
+            return;
+        }
+
+        const index = this.logsInContact.findIndex(log => log === logComponent);
+
+        if (index !== -1) {
+            this.logsInContact.splice(index, 1);
+            console.log(`Бревно удалено. Осталось бревен: ${this.logsInContact.length}`);
+
+            if (this.logsInContact.length === 0) {
+                console.log("Test");
             }
         }
     }
